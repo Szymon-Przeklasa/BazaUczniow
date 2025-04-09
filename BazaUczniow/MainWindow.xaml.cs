51a7920a44358866a4c3eb3a68193e04eedc362a
Szymon-Przeklasa <112714753+Szymon-Przeklasa@users.noreply.github.com>
2025-04-09 13:02:04 +0200

    Add files via upload

---
+++ BazaUczniow/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BazaUczniow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Uczen
+        {
+            public string list_PESEL { get; set; }
+            public string list_Imie { get; set; }
+            public string list_Imie2 { get; set; }
+            public string list_Nazwisko { get; set; }
+            public string list_DataUrodzenia { get; set; }
+            public string list_NumerTelefonu { get; set; }
+            public string list_Adres { get; set; }
+            public string list_Miejscowosc { get; set; }
+            public string list_KodPocztowy { get; set; }
+
+            public Uczen()
+            {
+
+            }
+
+            public void Przypisz(string _listPESEL, string _listImie, string _listImie2, string _listNazwisko, string _listDataUrodzenia, string _listNumerTelefonu, string _listAdres, string _listMiejscowosc, string _listKodPocztowy)
+            {
+                list_PESEL = _listPESEL;
+                list_Imie = _listImie;
+                list_Imie2 = _listImie2;
+                list_Nazwisko = _listNazwisko;
+                list_DataUrodzenia = _listDataUrodzenia;
+                list_NumerTelefonu = _listNumerTelefonu;
+                list_Adres = _listAdres;
+                list_Miejscowosc = _listMiejscowosc;
+                list_KodPocztowy = _listKodPocztowy;
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void przycisk_Click(object sender, RoutedEventArgs e)
+        {
+            var okno = new Window1();
+            okno.ShowDialog();
+        }
+    }
+}
---
+++ BazaUczniow/Window1.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BazaUczniow
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public string PESEL_Window1;
+        public string Imie_Window1;
+        public string Imie2_Window1;
+        public string Nazwisko_Window1;
+        public string DataUrodzenia_Window1;
+        public string NumerTelefonu_Window1;
+        public string Adres_Window1;
+        public string Miejscowosc_Window1;
+        public string KodPocztowy_Window1;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            PESEL_Window1 = PESEL_input.Text;
+            Imie_Window1 = Imie_input.Text;
+            Imie2_Window1 = Imie2_input.Text;
+            Nazwisko_Window1 = Nazwisko_input.Text;
+            DataUrodzenia_Window1 = DataUrodzenia_input.Text;
+            NumerTelefonu_Window1 = NumerTelefonu_input.Text;
+            Adres_Window1 = Adres_input.Text;
+            Miejscowosc_Window1 = Miejscowosc_input.Text;
+            KodPocztowy_Window1 = KodPocztowy_input.Text;
+        }
+        private void Anuluj_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(PESEL_input.Text) ||
+                !String.IsNullOrEmpty(Imie_input.Text) ||
+                !String.IsNullOrEmpty(Imie2_input.Text) ||
+                !String.IsNullOrEmpty(Nazwisko_input.Text) ||
+                !String.IsNullOrEmpty(DataUrodzenia_input.Text) ||
+                !String.IsNullOrEmpty(NumerTelefonu_input.Text) ||
+                !String.IsNullOrEmpty(Adres_input.Text) ||
+                !String.IsNullOrEmpty(Miejscowosc_input.Text) ||
+                !String.IsNullOrEmpty(KodPocztowy_input.Text))
+            {
+                MessageBoxResult result = MessageBox.Show("Czy na pewno chcesz zamknąć okno bez zapisywania?", "Potwierdzenie", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    this.Close();
+                }
+                else
+                {
+
+                }
+            } else
+            {
+                this.Close();
+            }
+        }
+    }
+}
