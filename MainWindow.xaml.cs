651db80a387d596cb83f100fafadd1dae04f8bae
Szymon-Przeklasa <112714753+Szymon-Przeklasa@users.noreply.github.com>
2025-05-11 12:44:49 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.IO;
+
+
+namespace BazaUczniow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Uczen
+        {
+            public string list_PESEL { get; set; }
+            public string list_Imie { get; set; }
+            public string list_Imie2 { get; set; }
+            public string list_Nazwisko { get; set; }
+            public string list_DataUrodzenia { get; set; }
+            public string list_NumerTelefonu { get; set; }
+            public string list_Adres { get; set; }
+            public string list_Miejscowosc { get; set; }
+            public string list_KodPocztowy { get; set; }
+
+            public Uczen()
+            {
+
+            }
+
+            public void Przypisz(string _listPESEL, string _listImie, string _listImie2, string _listNazwisko, string _listDataUrodzenia, string _listNumerTelefonu, string _listAdres, string _listMiejscowosc, string _listKodPocztowy)
+            {
+                list_PESEL = _listPESEL;
+                list_Imie = _listImie;
+                list_Imie2 = _listImie2;
+                list_Nazwisko = _listNazwisko;
+                list_DataUrodzenia = _listDataUrodzenia;
+                list_NumerTelefonu = _listNumerTelefonu;
+                list_Adres = _listAdres;
+                list_Miejscowosc = _listMiejscowosc;
+                list_KodPocztowy = _listKodPocztowy;
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void wczytaj_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Pliki CSV z separatorem (,) |*.csv|Pliki CSV z separatorem (;) |*.csv";
+            openFileDialog.Title = "Otwórz plik CSV";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                listview.Items.Clear();
+                string filePath = openFileDialog.FileName;
+                int selectedFilterIndex = openFileDialog.FilterIndex;
+                string delimiter = ";";
+                if (selectedFilterIndex == 1)
+                {
+                    delimiter = ",";
+                }
+                Encoding encoding = Encoding.UTF8;
+                if (File.Exists(filePath))
+                {
+                    var lines = File.ReadAllLines(filePath, encoding);
+                    foreach (var line in lines)
+                    {
+                        string[] columns = line.Split(delimiter);
+                        if (columns != null)
+                        {
+                            var uczen = new Uczen();
+                            uczen.list_PESEL = columns.ElementAtOrDefault(0);
+                            uczen.list_Imie = columns.ElementAtOrDefault(1);
+                            uczen.list_Imie2 = columns.ElementAtOrDefault(2);
+                            uczen.list_Nazwisko = columns.ElementAtOrDefault(3);
+                            uczen.list_DataUrodzenia = columns.ElementAtOrDefault(4);
+                            uczen.list_NumerTelefonu = columns.ElementAtOrDefault(5);
+                            uczen.list_Adres = columns.ElementAtOrDefault(6);
+                            uczen.list_Miejscowosc = columns.ElementAtOrDefault(7);
+                            uczen.list_KodPocztowy = columns.ElementAtOrDefault(8);
+                            listview.Items.Add(uczen);
+                        }
+                    }
+                }
+            }
+
+        }
+        private void zapisz_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Pliki CSV z separatorem (,) |*.csv|Pliki CSV z separatorem (;) |*.csv";
+            saveFileDialog.Title = "Zapisz jako plik CSV";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filePath = saveFileDialog.FileName;
+                string delimiter = ";";
+                if (saveFileDialog.FilterIndex == 1)
+                {
+                    delimiter = ",";
+                }
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (Uczen item in listview.Items)
+                    {
+                        var row = $"{item.list_PESEL}{delimiter}{item.list_Imie}" +
+                        $"{delimiter}{item.list_Imie2}{delimiter}{item.list_Nazwisko}" +
+                        $"{delimiter}{item.list_DataUrodzenia}{delimiter}{item.list_NumerTelefonu}" +
+                        $"{delimiter}{item.list_Adres}{delimiter}{item.list_Miejscowosc}{delimiter}{item.list_KodPocztowy}";
+                        writer.WriteLine(row);
+                    }
+                }
+            }
+        }
+        private void dodaj_Click(object sender, RoutedEventArgs e)
+        {
+            var okno = new Window1();
+            okno.ShowDialog();
+
+            string pesel = okno.PESEL_Window1;
+            string imie = okno.Imie_Window1;
+            string imie2 = okno.Imie2_Window1;
+            string nazwisko = okno.Nazwisko_Window1;
+            string data_urodzenia = okno.DataUrodzenia_Window1;
+            string nr_telefonu = okno.NumerTelefonu_Window1;
+            string adres = okno.Adres_Window1;
+            string miejscowosc = okno.Miejscowosc_Window1;
+            string kod_pocztowy = okno.KodPocztowy_Window1;
+
+            var uczen = new Uczen();
+            uczen.Przypisz(pesel, imie, imie2, nazwisko, data_urodzenia, nr_telefonu, adres, miejscowosc, kod_pocztowy);
+            if(okno.a == true)
+            {
+
+            } else
+            {
+                listview.Items.Add(uczen);
+            }
+    }
+        private void usun_Click(object sender, RoutedEventArgs e)
+        {
+            while (listview.SelectedItems.Count > 0)
+            {
+                listview.Items.Remove(listview.SelectedItems[0]);
+            }
+        }
+
+        private void info_Click(object sender, RoutedEventArgs e)
+        {
+            var info = new Info();
+            info.ShowDialog();
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BazaUczniow
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public string PESEL_Window1;
+        public string Imie_Window1;
+        public string Imie2_Window1;
+        public string Nazwisko_Window1;
+        public string DataUrodzenia_Window1;
+        public string NumerTelefonu_Window1;
+        public string Adres_Window1;
+        public string Miejscowosc_Window1;
+        public string KodPocztowy_Window1;
+        public bool a = false;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            string DuzaLitera(string slowo)
+            {
+                if (slowo != Adres_input.Text)
+                {
+                    slowo = slowo.Replace(" ", "");
+                }
+                if (slowo.Length == 0) return "";
+                string duza = slowo[0].ToString().ToUpper();
+                string reszta = slowo.Substring(1);
+                slowo = duza + reszta;
+                return slowo;
+            }
+
+            string Telefon(string numer_telefonu)
+            {
+                numer_telefonu = numer_telefonu.Replace(" ", "");
+                if (!numer_telefonu.StartsWith("+48"))
+                {
+                    numer_telefonu = "+48" + numer_telefonu;
+                }
+                return numer_telefonu;
+            }
+
+            bool SprawdzPesel(string pesel)
+            {
+                for(int i = 0; i < pesel.Length; i++)
+                {
+                    if (pesel.Length != 11 || !char.IsDigit(pesel[i]))
+                    {
+                        return false;
+                    }
+                }
+
+                int rok = int.Parse(pesel.Substring(0, 2));
+                int miesiac = int.Parse(pesel.Substring(2, 2));
+                int dzien = int.Parse(pesel.Substring(4, 2));
+
+                if (miesiac >= 1 && miesiac <= 12)
+                {
+                    rok += 1900;
+                }
+                else if (miesiac >= 21 && miesiac <= 32)
+                {
+                    miesiac -= 20;
+                    rok += 2000;
+                }
+                else if (miesiac >= 41 && miesiac <= 52)
+                {
+                    miesiac -= 40;
+                    rok += 2100;
+                }
+                else if (miesiac >= 61 && miesiac <= 72)
+                {
+                    miesiac -= 60;
+                    rok += 2200;
+                }
+                else if (miesiac >= 81 && miesiac <= 92)
+                {
+                    miesiac -= 80;
+                    rok += 1800;
+                }
+                else
+                {
+                    return false;
+                }
+
+                if (dzien < 1 || dzien > DateTime.DaysInMonth(rok, miesiac))
+                {
+                    return false;
+                }
+
+                int[] wagi = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+                int suma = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    suma += int.Parse(pesel[i].ToString()) * wagi[i];
+                }
+                int kontrolna = (10 - (suma % 10)) % 10;
+
+                return kontrolna == int.Parse(pesel[10].ToString());
+            }
+
+            int poprawne = 0;
+            bool wszystkoOk = true;
+
+            string tempPesel = "";
+            string tempImie = "";
+            string tempImie2 = "-";
+            string tempNazwisko = "";
+            string tempDataUrodzenia = "";
+            string tempTelefon = "-";
+            string tempAdres = "";
+            string tempMiejscowosc = "";
+            string tempKodPocztowy = "";
+
+            if (PESEL_input.Text == "")
+            {
+                PESEL_input.Background = Brushes.Red;
+                wszystkoOk = false;
+            }
+            else if (SprawdzPesel(PESEL_input.Text))
+            {
+                tempPesel = PESEL_input.Text;
+                PESEL_input.Background = Brushes.White;
+                poprawne++;
+            }
+            else
+            {
+                PESEL_input.Background = Brushes.Red;
+                wszystkoOk = false;
+            }
+
+            if (Imie_input.Text == "")
+            {
+                Imie_input.Background = Brushes.Red;
+                wszystkoOk = false;
+            }
+            else
+            {
+                tempImie = DuzaLitera(Imie_input.Text);
+                Imie_input.Background = Brushes.White;
+                poprawne++;
+            }
+
+            if (Imie2_input.Text != "")
+            {
+                tempImie2 = DuzaLitera(Imie2_input.Text);
+            }
+
+            if (Nazwisko_input.Text == "")
+            {
+                Nazwisko_input.Background = Brushes.Red;
+                wszystkoOk = false;
+            }
+            else
+            {
+                tempNazwisko = DuzaLitera(Nazwisko_input.Text);
+                Nazwisko_input.Background = Brushes.White;
+                poprawne++;
+            }
+
+            if (DataUrodzenia_input.Text == "")
+            {
+                DataUrodzenia_input.Background = Brushes.Red;
+                wszystkoOk = false;
+            }
+            else
+            {
+                tempDataUrodzenia = DataUrodzenia_input.Text;
+                DataUrodzenia_input.Background = Brushes.White;
+                poprawne++;
+            }
+
+            if (NumerTelefonu_input.Text != "")
+            {
+                tempTelefon = Telefon(NumerTelefonu_input.Text);
+            }
+
+            if (Adres_input.Text == "")
+            {
+                Adres_input.Background = Brushes.Red;
+                wszystkoOk = false;
+            }
+            else
+            {
+                tempAdres = DuzaLitera(Adres_input.Text);
+                Adres_input.Background = Brushes.White;
+                poprawne++;
+            }
+
+            if (Miejscowosc_input.Text == "")
+            {
+                Miejscowosc_input.Background = Brushes.Red;
+                wszystkoOk = false;
+            }
+            else
+            {
+                tempMiejscowosc = DuzaLitera(Miejscowosc_input.Text);
+                Miejscowosc_input.Background = Brushes.White;
+                poprawne++;
+            }
+
+            if (KodPocztowy_input.Text == "")
+            {
+                KodPocztowy_input.Background = Brushes.Red;
+                wszystkoOk = false;
+            }
+            else
+            {
+                tempKodPocztowy = KodPocztowy_input.Text;
+                KodPocztowy_input.Background = Brushes.White;
+                poprawne++;
+            }
+
+            if (poprawne == 7 && wszystkoOk)
+            {
+                PESEL_Window1 = tempPesel;
+                Imie_Window1 = tempImie;
+                Imie2_Window1 = tempImie2;
+                Nazwisko_Window1 = tempNazwisko;
+                DataUrodzenia_Window1 = tempDataUrodzenia;
+                NumerTelefonu_Window1 = tempTelefon;
+                Adres_Window1 = tempAdres;
+                Miejscowosc_Window1 = tempMiejscowosc;
+                KodPocztowy_Window1 = tempKodPocztowy;
+
+                this.Close();
+            }
+        }
+
+        private void Anuluj_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(PESEL_input.Text) ||
+                !String.IsNullOrEmpty(Imie_input.Text) ||
+                !String.IsNullOrEmpty(Imie2_input.Text) ||
+                !String.IsNullOrEmpty(Nazwisko_input.Text) ||
+                !String.IsNullOrEmpty(DataUrodzenia_input.Text) ||
+                !String.IsNullOrEmpty(NumerTelefonu_input.Text) ||
+                !String.IsNullOrEmpty(Adres_input.Text) ||
+                !String.IsNullOrEmpty(Miejscowosc_input.Text) ||
+                !String.IsNullOrEmpty(KodPocztowy_input.Text))
+            {
+                MessageBoxResult result = MessageBox.Show("Czy na pewno chcesz zamknąć okno bez zapisywania?", "Potwierdzenie", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    a = true;
+                    this.Close();
+                }
+                else
+                {
+                    a = false;
+                }
+            } else
+            {
+                a = true;
+                this.Close();
+            }
+        }
+    }
+}
